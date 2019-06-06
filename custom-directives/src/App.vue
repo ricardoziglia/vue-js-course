d75d5003e25686c538c94d90ab4244f8f9961128
ricardorohnelt@gmail.com <ricardorohnelt@gmail.com>
2019-06-06 18:20:59 -0300

    [directive] Created custom `v-on` directive.

---
+++ custom-directives/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-ziglia-on:click="clicked">Click me</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            clicked() {
+                alert('it works');
+            }
+        },
+        directives: {
+            'ziglia-on': {
+                bind(el, binding, vnode) {
+                    var eventType = binding.arg;
+                    var fn = binding.value;
+                    if (typeof fn === "function") {
+                        el.addEventListener(eventType, fn);
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
